5ce1f750d33b038eef4a21a53cbfb9933c640a82
Avinash Kadimisetty <kavinash366@gmail.com>
2018-10-08 00:23:42 -0500

    Added code for architecture of the network

--- ConvolutionalNeuralNetwork/dataloader.py
+++ ConvolutionalNeuralNetwork/dataloader.py
@@ -13,19 +13,21 @@ Created on Fri Oct  5 04:02:43 2018
 def generate_loaders(data_directory, bs):
     
     # List out all the data augmentation techniques needed using transforms.
-    data_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                     transforms.ColorJitter(),
-                                     transforms.toTensor()])
+    train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                           transforms.ColorJitter(),
+                                           transforms.toTensor()])
+    
+    test_transforms = transforms.Compose([transforms.toTensor()])
     
     # Download the datasets from PyTorch
     train_dataset = torchvision.datasets.MNIST(root = ".",
                                                train = True,
                                                download = True,
-                                               transforms = data_transforms)
+                                               transforms = train_transforms)
     test_dataset = torchvision.datasets.MNIST(root = ".",
                                               train = False,
                                               download = False,
-                                              transforms = transforms.toTensor())
+                                              transforms = test_transforms)
     
     # Create loaders
     train_loader = torch.utils.data.DataLoader(data = train_dataset,
---
+++ ConvolutionalNeuralNetwork/model_architecture.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 04:14:31 2018
+
+@author: avinash
+"""
+
+# =============================================================================
+# 
+# =============================================================================
+
+class CNN(nn.Module):
+    
+    def __init__(self, num_classes = 10):
+        super(CNN_MNIST, self).__init__()
+        self.layer1 = 
+    
+    def forward_step(self, inp):
+        
