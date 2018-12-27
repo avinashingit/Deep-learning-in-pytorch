d5ac064b8c14a1ffd774bd86503d8b9db72d6051
Avinash Kadimisetty <kavinash366@gmail.com>
2018-12-27 12:57:50 -0600

    CNN Code is written

--- ConvolutionalNeuralNetwork/dataloader.py
+++ ConvolutionalNeuralNetwork/dataloader.py
@@ -1,39 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  5 04:02:43 2018
+# @Author: Avinash Kadimisetty <avinash>
+# @Date:   2018-12-27T12:30:53-06:00
+# @Project: ConvolutionalNeuralNetwork
+# @Filename: dataloader.py
+# @Last modified by:   avinash
+# @Last modified time: 2018-12-27T12:53:15-06:00
 
-@author: Avinash Kadimisetty
-"""
 
 # =============================================================================
-# This function loads the dataset from Pytorch datasets and returns a 
+# This function loads the dataset from Pytorch datasets and returns a
 # a dataloader which can be used while training and testing
 # =============================================================================
-def generate_loaders(data_directory, bs):
-    
+
+import torch
+import torchvision.transforms as transforms
+import torchvision
+
+
+def generate_loaders(data_directory, batchsize):
+
     # List out all the data augmentation techniques needed using transforms.
     train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
                                            transforms.ColorJitter(),
-                                           transforms.toTensor()])
-    
-    test_transforms = transforms.Compose([transforms.toTensor()])
-    
+                                           transforms.ToTensor()])
+
+    test_transforms = transforms.Compose([transforms.ToTensor()])
+
     # Download the datasets from PyTorch
-    train_dataset = torchvision.datasets.MNIST(root = ".",
-                                               train = True,
-                                               download = True,
-                                               transforms = train_transforms)
-    test_dataset = torchvision.datasets.MNIST(root = ".",
-                                              train = False,
-                                              download = False,
-                                              transforms = test_transforms)
-    
+    train_dataset = torchvision.datasets.CIFAR10(root=data_directory,
+                                                 train=True,
+                                                 download=True,
+                                                 transform=train_transforms)
+    test_dataset = torchvision.datasets.CIFAR10(root=data_directory,
+                                                train=False,
+                                                download=True,
+                                                transform=test_transforms)
+
     # Create loaders
-    train_loader = torch.utils.data.DataLoader(data = train_dataset,
-                                               batch_size = bs,
-                                               shuffle = True)
-    test_loader = torch.utils.data.DataLoader(data = test_dataset,
-                                              batch_size = bs,
-                                              shuffle = False)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batchsize,
+                                               shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batchsize,
+                                              shuffle=False)
     return train_loader, test_loader
